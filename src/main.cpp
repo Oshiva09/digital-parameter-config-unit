8bd9f3a8aefe5eee9eb6e0457348e52729875e29
Oshiva09 <oshiva.singh_cs.aiml24@gla.ac.in>
2026-02-11 13:59:20 +0530

    initiate serial monitor and oled with begin function

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,12 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 void setup() {
+    Serial.begin(9600);
+        if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+            Serial.println(F("OLED not found"));
+            while (true);
+        }
+
 // write your initialization code here
 }
 
